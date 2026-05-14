df28fff3be21a5b356f9320eca3742734b56d667
vinodwaghmare <45902596+vinodwaghmare@users.noreply.github.com>
2026-05-14 16:21:41 +0000

    Deploying to gh-pages from @ vinodwaghmare/vinodwaghmare.github.io@b39dbf80d204345327eb66686daea5055d556b05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -191,9 +191,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-livecareai",
+          title: 'LiveCareAI',
+          description: "Real-time AI patient monitoring via smartwatch vitals on Kafka streaming",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
