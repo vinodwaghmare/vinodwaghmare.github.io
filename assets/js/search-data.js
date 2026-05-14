dd69e08c7a1a373a6fec150805b9b4cc9b87cb2b
vinodwaghmare <45902596+vinodwaghmare@users.noreply.github.com>
2026-05-14 19:46:06 +0000

    Deploying to gh-pages from @ vinodwaghmare/vinodwaghmare.github.io@cdde48899ea0acf669b99997f51b78b47126ac9c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "Download or view my CV below.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
